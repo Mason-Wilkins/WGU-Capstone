d4dc59119ee563a195acadc5137a9ea8466d6a27
Mason Wilkins <mwilkins17@gmail.com>
2025-09-26 13:21:21 -0600

    Added server and gitignore

--- server.py
+++ server.py
@@ -7,3 +7,8 @@ FLASK_APP_SECRET_KEY = os.environ['FLASK_APP_SECRET_KEY']
 
 app = Flask(__name__)
 app.secret_key = FLASK_APP_SECRET_KEY
+
+# @app.route("/")
+# def index():
+#     """Render Home page"""
+#     return render_template('index.html')
